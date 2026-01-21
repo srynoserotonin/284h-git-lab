0f29b9682e9210b63be3fabb113b79ceb897acc3
srynoserotonin <115327416+srynoserotonin@users.noreply.github.com>
2026-01-21 15:26:05 -0600

    Update test_git_lab.py

--- test_git_lab.py
+++ test_git_lab.py
@@ -4,7 +4,7 @@ from git_lab import (
     celsius_to_fahrenheit,
     is_palindrome,
     calculate_triangle_area,
-    is_prime,
+    is_prime, add_numbers
 )
 import math
 
@@ -47,5 +47,11 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(is_prime(31))
         self.assertFalse(is_prime(100))
 
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+
 if __name__ == '__main__':
     unittest.main()
+
+   

1863dbdd81491c00f84bb3a2e9c260c5733a96e1
srynoserotonin <115327416+srynoserotonin@users.noreply.github.com>
2026-01-21 15:34:08 -0600

    Update git_lab.py

--- git_lab.py
+++ git_lab.py
@@ -61,5 +61,5 @@ def is_prime(n):
     return True
 
 def add_numbers(a, b):
-    """Returns the sum of two numbers."""
-    return a - b
+    """Returns the correct sum of two numbers."""
+    return a + b
